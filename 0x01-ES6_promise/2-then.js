76a60c6aba5e32d3d75245027bc427bb80e52e19
Iamloumie <lawaladedamolaolumide@gmail.com>
2025-04-16 13:15:42 +0100

    task 3

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,14 +1,13 @@
 /**
- * This function takes a promise as an argument and
- * returns an object with the following properties:
+ * Attaches handlers to a given promise to handle resolution, rejection, and logging.
+ *
+ * @param {Promise} promise - The promise to handle.
+ * @returns {Promise} A promise that resolves with an object {status: 200, body: 'success'}
+ *                    if the input promise resolves, or with an empty Error object if it rejects.
  */
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then(() => ({
-      status: 200,
-      body: 'Success',
-    }))
+    .then(() => ({ status: 200, body: "success" }))
     .catch(() => new Error())
-    // .finally() is called regardless of the promise's outcome
-    .finally(() => console.log('Got a response from the API'));
+    .finally(() => console.log("Got a response from the API"));
 }
