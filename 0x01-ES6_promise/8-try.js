67ab3fac81d9ea617028a18ad51c620f736a054e
Iamloumie <lawaladedamolaolumide@gmail.com>
2025-04-16 14:28:18 +0100

    done

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,9 @@
+/**
+ * Handles division by zero error */
+
+export default function divideFunction(numerator, denominator) {
+  if (denominator === 0) {
+    throw new Error('cannot divide by 0.');
+  }
+  return numerator / denominator;
+}
