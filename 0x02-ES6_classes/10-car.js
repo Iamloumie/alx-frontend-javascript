cf082a70a4ab3cbcfca933766f610032b3abfcb4
Iamloumie <lawaladedamolaolumide@gmail.com>
2025-04-17 15:29:05 +0100

    fixed

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -2,50 +2,15 @@ export default class Car {
   constructor(brand, motor, color) {
     if (typeof brand !== 'string') {
       throw new TypeError('brand must be a string');
-    }
-    if (typeof motor !== 'string') {
+    } else if (typeof motor !== 'string') {
       throw new TypeError('motor must be a string');
-    }
-    if (typeof color !== 'string') {
+    } else if (typeof color !== 'string') {
       throw new TypeError('color must be a string');
     }
 
     this._brand = brand;
     this._motor = motor;
     this._color = color;
-  }
-
-  get brand() {
-    return this._brand;
-  }
-
-  set brand(newBrand) {
-    if (typeof newBrand !== 'string') {
-      throw new TypeError('brand must be a string');
-    }
-    this._brand = newBrand;
-  }
-
-  get motor() {
-    return this._motor;
-  }
-
-  set motor(newMotor) {
-    if (typeof newMotor !== 'string') {
-      throw new TypeError('motor must be a string');
-    }
-    this._motor = newMotor;
-  }
-
-  get color() {
-    return this._color;
-  }
-
-  set color(newColor) {
-    if (typeof newColor !== 'string') {
-      throw new TypeError('color must be a string');
-    }
-    this._color = newColor;
   }
 
   cloneCar() {
